2d04d550ea40a43e0383a328d3dd591ae357f0a8
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2023-04-26 01:52:59 -0700

    add errors with context instead of returning nil

--- dea.go
+++ dea.go
@@ -1,9 +1,16 @@
 package dea
 
 import (
+	"errors"
 	"net"
 	"regexp"
 	"strings"
+)
+
+var (
+	ErrDisposableEmail          = errors.New("email is a disposable email address")
+	ErrInvalidEmail             = errors.New("not valid email format")
+	ErrCouldNotLookupSPFRecords = errors.New("could not lookup SPF records")
 )
 
 func IsDisposableEmail(email string) (bool, error) {
@@ -18,6 +25,8 @@ func IsDisposableEmail(email string) (bool, error) {
 					return false, nil
 				}
 			}
+			// If provider is not explicitly allowed, return true
+			return true, ErrDisposableEmail
 		}
 	}
 
@@ -33,19 +42,19 @@ func IsDisposableEmail(email string) (bool, error) {
 	// TODO: Do more indepth checks against, MX and SPF records.
 	parts := strings.Split(email, "@")
 	if len(parts) != 2 {
-		return true, nil
+		return true, ErrInvalidEmail
 	}
 	domain := parts[1]
 
 	txtRecords, err := net.LookupTXT(domain)
 	if err != nil {
-		return true, nil
+		return true, ErrCouldNotLookupSPFRecords
 	}
 
 	for _, spfRecord := range txtRecords {
 		if strings.HasPrefix(spfRecord, "v=spf1 ") {
 			if len(spfRecord) < 25 {
-				return true, nil
+				return true, ErrDisposableEmail
 			}
 		}
 	}
@@ -53,7 +62,7 @@ func IsDisposableEmail(email string) (bool, error) {
 	// Step 3: check for email addresses that contain random strings of characters or misspelled words
 	pattern := regexp.MustCompile(`^([a-z0-9._%+\-]+|([a-z0-9]+[.\-_])*[a-z0-9]+)@(10|anon|example|guerrilla|fake|disposable|temp)([.\-_][a-z]{2,})+$`)
 	if pattern.MatchString(email) {
-		return true, nil
+		return true, ErrDisposableEmail
 	}
 
 	return false, nil
