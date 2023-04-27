70e7915b2bd18cfb607cf99b776dc70e7f6facff
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2023-04-26 22:26:48 -0700

    update

--- dea_test.go
+++ dea_test.go
@@ -11,7 +11,7 @@ func TestIsDisposableEmail(t *testing.T) {
 		inputEmail string
 		want       bool
 	}{
-		{name: "Check against valid email", inputEmail: "demo@gmail.com", want: false},
+		{name: "Check against valid email", inputEmail: "prabesh66@gmail.com", want: false},
 		{name: "Check against invalid email", inputEmail: "demo@027168.com", want: true},
 		{name: "Check against random string", inputEmail: "asdsadasdkjh@asdjkhasd.com", want: true},
 		{name: "Check against empty strng", inputEmail: "", want: true},
